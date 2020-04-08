d2a9f233ed30638bbab12b12d76f8cadc717e927
ram1123 <ramkrishna.sharma71@gmail.com>
2020-04-08 17:42:36 -0500

    updated condor jobs

--- RunGENSIM_condor.sh
+++ RunGENSIM_condor.sh
@@ -4,7 +4,7 @@ echo "Running on: `uname -a`"
 echo "System software: `cat /etc/redhat-release`"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-OUTDIR=root://cmseos.fnal.gov//store/user/rasharma/aashaqTemp/
+OUTDIR=root://cmseos.fnal.gov//store/user/rasharma/double-higgs/SignalSample/GF_HH_Benchmark1/
 
 echo "======="
 ls
@@ -23,19 +23,19 @@ echo "List all root files = "
 ls *.root
 echo "List all files"
 ls 
-#echo "xrdcp output for condor"
-#for FILE in *inLHE*.root
-#do
-#  echo "xrdcp -f ${FILE} ${OUTDIR}/out_inLHE_${1}_${2}.root"
-#  xrdcp -f ${FILE} ${OUTDIR}/out_inLHE_${1}_${2}.root 2>&1
-#  XRDEXIT=$?
-#  if [[ $XRDEXIT -ne 0 ]]; then
-#    rm *.root
-#    echo "exit code $XRDEXIT, failure in xrdcp"
-#    exit $XRDEXIT
-#  fi
-#  rm ${FILE}
-#done
+echo "xrdcp output for condor"
+for FILE in *LHEGS-01725.root
+do
+  echo "xrdcp -f ${FILE} ${OUTDIR}/out_inLHE_${1}_${2}.root"
+  xrdcp -f ${FILE} ${OUTDIR}/out_inLHE_${1}_${2}.root 2>&1
+  XRDEXIT=$?
+  if [[ $XRDEXIT -ne 0 ]]; then
+    rm *.root
+    echo "exit code $XRDEXIT, failure in xrdcp"
+    exit $XRDEXIT
+  fi
+  rm ${FILE}
+done
 date
 echo "+=============================="
 
@@ -47,9 +47,9 @@ cd CMSSW_10_2_5/src
 echo $PWD
 eval `scram runtime -sh`
 cd -
-cmsRun HIG-RunIISummer16DR80Premix-01616_1_cfg.py $*
-cmsRun HIG-RunIISummer16DR80Premix-01616_2_cfg.py $*
-cmsRun HIG-RunIISummer16MiniAODv2-01613_1_cfg.py $*
+cmsRun B2G-RunIIAutumn18DRPremix-02890_1_cfg.py  
+cmsRun B2G-RunIIAutumn18DRPremix-02890_2_cfg.py 
+cmsRun B2G-RunIIAutumn18MiniAOD-02887_1_cfg.py
 echo "List all root files = "
 ls *.root
 echo "List all files"
@@ -58,10 +58,38 @@ echo "+=============================="
 
 # copy output to eos
 echo "xrdcp output for condor"
-for FILE in HIG-RunIISummer16MiniAODv2-*.root 
+for FILE in B2G*MiniAOD*.root 
 do
   echo "xrdcp -f ${FILE} ${OUTDIR}/out_miniAOD_${1}_${2}.root"
   xrdcp -f ${FILE} ${OUTDIR}/out_miniAOD_${1}_${2}.root 2>&1
+  XRDEXIT=$?
+  if [[ $XRDEXIT -ne 0 ]]; then
+    rm *.root
+    echo "exit code $XRDEXIT, failure in xrdcp"
+    exit $XRDEXIT
+  fi
+  rm ${FILE}
+done
+date
+
+
+echo "+=============================="
+echo "==> Running NanoAOD..."
+eval `scramv1 project CMSSW CMSSW_10_2_18`
+cd CMSSW_10_2_18/src
+echo $PWD
+eval `scram runtime -sh`
+cd -
+cmsRun B2G-RunIIAutumn18NanoAODv6-01916_1_cfg.py
+echo "List all root files = "
+ls *.root
+echo "+=============================="
+# copy output to eos
+echo "xrdcp output for condor"
+for FILE in B2G*NanoAOD*.root 
+do
+  echo "xrdcp -f ${FILE} ${OUTDIR}/out_miniAOD_${1}_${2}.root"
+  xrdcp -f ${FILE} ${OUTDIR}/out_nanoAOD_${1}_${2}.root 2>&1
   XRDEXIT=$?
   if [[ $XRDEXIT -ne 0 ]]; then
     rm *.root
